6ceb0af6b0db199371436345f6f1b13b59677d48
Norcissus <norcissus@foxmail.com>
2014-04-22 16:18:36 +0800

    change build script.

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -x
+PDIR=$(pwd)
 autoconf
-autoreconf -is && \
-./configure CXXFLAGS='-O0 -ggdb' --enable-shared && \
+set -e
+autoreconf -is
+mkdir -p /tmp/ext/built
+mkdir -p /tmp/ext/inst
+cd /tmp/ext/built
+$PDIR/configure CXXFLAGS='-O0 -ggdb' --enable-shared --srcdir=$PDIR  --prefix=/tmp/ext/inst
 make -j check
