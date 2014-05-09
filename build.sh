ed9873e591bd17cec9f14c6166739acb8fb3a3cb
Xu Zhe <xuzhev@qq.com>
2014-05-09 20:14:12 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,8 @@ BDIR="/tmp/eut"
 autoconf
 set -e
 autoreconf -is
-rm -rf $BDIR
 mkdir -p $BDIR/built
 mkdir -p $BDIR/inst
 cd $BDIR/built
-$SRCDIR/configure CXXFLAGS='-O0 -ggdb' --enable-shared --srcdir=$SRCDIR --prefix=$BDIR/inst
+$SRCDIR/configure CXXFLAGS='-O0 -ggdb' --enable-shared --srcdir=$SRCDIR --prefix=$SRCDIR/../usr --includedir=$SRCDIR/../usr/include/eut
 make -j check
