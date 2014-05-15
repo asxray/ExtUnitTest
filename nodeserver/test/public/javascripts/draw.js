8d902d137723b0b55ca58c271b16b709dea16e2c
qash1 <qash1@dhcp-10-19-192-234.nvidia.com>
2014-05-15 06:11:46 -0400

    modified table style, added button to page up and down:x

--- nodeserver/test/public/javascripts/draw.js
+++ nodeserver/test/public/javascripts/draw.js
@@ -1,3 +1,38 @@
+var tab;
+var recordsPerPage;
+var page;
+var rootelement;
+var header;
+function Next()
+{
+	if(page<Math.ceil(tab.length/recordsPerPage))
+	{
+		rootelement.innerHTML="";
+		addHeader(rootelement,header);
+		for(var k=page*recordsPerPage;k<(page+1)*recordsPerPage;k++)
+		{
+			setRow(rootelement,tab[k]);
+		};
+		page++;
+	}
+}
+
+function Back()
+{
+	if(page>=2)
+	{
+		rootelement.innerHTML="";
+		addHeader(rootelement,header);
+		for(var k=(page-2)*recordsPerPage;k<(page-1)*recordsPerPage;k++)
+		{
+			setRow(rootelement,tab[k]);
+		};
+	page--;
+	}
+};
+
+
+
 function getData()
 {
 var xmlhttp;
@@ -5,28 +40,29 @@ xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function(){
 if (xmlhttp.readyState==4 && xmlhttp.status==200)
 {
-document.getElementById("myDiv").innerHTML="";
+document.getElementById("myChart").onclick=function() {
+this.style.display="none";
+};
 
+
+rootelement=document.getElementById("myDiv");
+document.getElementById("myDiv").innerHTML="";
+recordsPerPage=1000;
+page=1;
 var record= JSON.parse(xmlhttp.responseText);
-var tab=new Array();
+tab=new Array();
 var idx=-1;
 var lastcase="";
-
 header=new Object();
 for(i=0;i<record.length;i++)
 {
 	header[record[i]["Time"]+"_"+record[i]["DriverVersion"]+"_"+record[i]["CL"]]=0;
 };
-var rootelement=document.getElementById("myDiv");
 addHeader(rootelement,header);
 for (i=0;i<record.length;i++)
 {
     if(record[i]["Casename"]!=lastcase)
     {	
-	if(idx>=0)
-	{
-		setRow(rootelement,tab[idx]);	
-	};
 	idx++;
 	tab[idx]=new Object();
 	tab[idx]["Casename"]=record[i]["Casename"];
@@ -48,19 +84,27 @@ for (i=0;i<record.length;i++)
     tab[idx][record[i]["Time"]+ "_"+record[i]["DriverVersion"]+"_"+record[i]["CL"]]=record[i]["Duration1"];
     
 };
-if(idx>=0)
+for(var k=0;k<page*recordsPerPage;k++)
 {
-	setRow(rootelement,tab[idx]);
+	setRow(rootelement,tab[k]);
 };
-
-	
 //var jsonHtmlTable = ConvertJsonToTable(tab, 'jsonTable', null, 'Download');
 
 //document.getElementById("myDiv").innerHTML=jsonHtmlTable ;
+
+
+
+}};
+xmlhttp.open("GET","/data?database=Library&table=cuFFT",true);
+xmlhttp.send(null);
+};
+
+
 function addHeader(root,row)
 {
         var p = document.createElement("div");
         p.className="Row";
+	p.setAttribute("id","header");
         var subp=document.createElement("div");
         subp.className="Cell";
         subp.appendChild(document.createTextNode("Casename"));
@@ -70,8 +114,14 @@ function addHeader(root,row)
         {
                 subp=document.createElement("div");
                 subp.className="Cell";
-                subp.appendChild(document.createTextNode(i));
-                p.appendChild(subp);
+		var ar=i.split("_");
+		for(var j in ar)
+		{
+			subp.innerHTML="date:"+ar[0]+"<br/>"
+                        +"driver:"+ar[1]+"<br/>"
+                        +"CL:"+ar[2];
+                	p.appendChild(subp);
+		};
         }
 	root.appendChild(p);
 };
@@ -90,10 +140,8 @@ function setRow(root,row){
 	root.appendChild(p);
 }
 
-$( "#myChart").click(function() {
-this.style.display="none";
 
-});
+
 
 function drawRowData(rowdata)
 {
@@ -138,63 +186,6 @@ var myNewChart = new Chart(ctx).Line(data,option);
 document.getElementById("myChart").style.top=(window.pageYOffset+event.clientY+10)+"px";
 document.getElementById("myChart").style.display="block";
 
-};
-
-
-$( "div >table > tbody >tr" ).click(function(event) {
-
-    stringa = $(this).attr("value");
-    record = JSON.parse(stringa);
-    var data = {
-	datasets : [
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(0,0,60,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-		}
-	]
-};
-
-
-    var d1=new Array();
-    var l=new Array();
-    var idx=0;
-for (var i in record)
-{
-console.log(i+":"+record[i]);
- if (i != "Casename")
- {
-  d1[idx]=record[i];
-  l[idx]=i;
-  idx++;
- }else
- {
-    data["datasets"][0]["title"]=record[i];
- }
-};
- data["datasets"][0]["data"]=d1;
-data["labels"]=l;
-var ctx = document.getElementById("myChart").getContext("2d");
-var max_=Math.max.apply(null,d1);
-var step=10;
-var option= 
-{
-    scaleOverride: true,
-    scaleSteps: step,
-    scaleStepWidth: Math.ceil(max_ / step * 1.5),
-    scaleStartValue: 0
-};
-var myNewChart = new Chart(ctx).Line(data,option);
-document.getElementById("myChart").style.top=(window.pageYOffset+event.clientY+10)+"px";
-
-document.getElementById("myChart").style.display="block";   
- 
-
-});
-}}
-xmlhttp.open("GET","/data?database=Library&table=cuFFT",true);
-xmlhttp.send(null)
 };
 
 
